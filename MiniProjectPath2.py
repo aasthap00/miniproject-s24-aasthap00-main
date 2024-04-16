e1b9078829fc9f5abb4041366bcb4d167664e4bc
jakescherer <jakescherer24@gmail.com>
2024-04-16 13:56:23 -0400

    bcb

--- MiniProjectPath2.py
+++ MiniProjectPath2.py
@@ -10,3 +10,6 @@ dataset_2['Manhattan Bridge']     = pandas.to_numeric(dataset_2['Manhattan Bridg
 dataset_2['Queensboro Bridge']    = pandas.to_numeric(dataset_2['Queensboro Bridge'].replace(',','', regex=True))
 dataset_2['Williamsburg Bridge']  = pandas.to_numeric(dataset_2['Williamsburg Bridge'].replace(',','', regex=True))
 #print(dataset_2.to_string()) #This line will print out your data
+
+
+mbv nb
