afe75ec6493b372c4beea244778da7f8a2de8315
jakescherer <jakescherer24@gmail.com>
2024-04-23 14:29:16 -0400

    step 3

--- MiniProjectPath2.py
+++ MiniProjectPath2.py
@@ -7,6 +7,7 @@ import numpy as np
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 
 
 
@@ -22,33 +23,23 @@ dataset_2['Williamsburg Bridge']  = pandas.to_numeric(dataset_2['Williamsburg Br
 dataset_2['Total']  = pandas.to_numeric(dataset_2['Total'].replace(',','', regex=True))
 #print(dataset_2.to_string()) #This line will print out your data
 
-
+# Question 1
 df = dataset_2[["Brooklyn Bridge", "Manhattan Bridge", "Queensboro Bridge", "Williamsburg Bridge", "Total"]]
-# print(df.corr())
+print(df.corr())
 
+# Question 2
 dataset_2['High Temp']  = pandas.to_numeric(dataset_2['High Temp'].replace(',','', regex=True))
 dataset_2['Low Temp']  = pandas.to_numeric(dataset_2['Low Temp'].replace(',','', regex=True))
 dataset_2['Precipitation']  = pandas.to_numeric(dataset_2['Precipitation'].replace(',','', regex=True))
 
 df1 = pandas.DataFrame(dataset_2[["High Temp", "Low Temp", "Precipitation", "Total"]])
-print(df1.head())
-print(df1.corr())
+# print(df1.head())
+# print(df1.corr())
 
 X = np.array(df1[["High Temp", "Low Temp", "Precipitation"]])
 Y = np.array(df1[["Total"]])
 
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
-
-# model = LinearRegression()
-# model.fit(X_train, y_train)
-
-# y_pred = model.predict(X_test)
-
-
-# MSE = mean_squared_error(y_test,y_pred)
-# print(MSE)
-
-
 poly = PolynomialFeatures(degree=3)
 
 X_ = poly.fit_transform(X_train)
@@ -61,16 +52,60 @@ MSE = mean_squared_error(y_pred,y_test)
 print(MSE)
 
 
-# model = Pipeline([
-#     ('polynomial_features', PolynomialFeatures(degree=3)),
-#     ('linear_regression', LinearRegression())
-# ])
+# Question 3
+df2 = np.array(dataset_2[["Total"]])
 
-# # Fit the model
-# model.fit(X_train,y_train)
+fri = []
+sat = []
+sun = []
+mon = []
+tues = []
+wed = []
+thurs = []
 
-# y_pred = model.predict(X_test)
+for idx, arr in enumerate(df2):
+    # print(f"Day {idx + 1}: {arr}")
+    match idx%7:
+        case 0: 
+            fri.append(arr)
+        case 1:
+            sat.append(arr)
+        case 2:
+            sun.append(arr)
+        case 3:
+            mon.append(arr)
+        case 4:
+            tues.append(arr)
+        case 5:
+            wed.append(arr)
+        case default:
+            thurs.append(arr)
 
-# MSE = mean_squared_error(y_pred,y_test)
-# print(MSE)
+def plotDay(dayTotal, day):
+    x = np.array(range(len(dayTotal)))
+
+    plt.grid(True)
+    plt.ylabel("Number of Bikers")
+    plt.xlabel("Time")
+    plt.title(f"{day} Bike Traffic")
+    
+    x2 = np.array(x).reshape((-1,1))
+    model = LinearRegression()
+    model.fit(x2,dayTotal)
+    r2 = '%.4f'%(model.score(x2,dayTotal))
+    print(f"{day} R^2 Value: {r2}")
+
+    pred = model.predict(x2)
+    plt.plot(x,dayTotal,'-b',marker='o')
+    plt.plot(x2,pred,'--k',label=f"R^2 Value: {r2}")
+    plt.legend(loc="lower right")
+    plt.show()
+
+plotDay(fri, "Friday")
+plotDay(sat, "Saturday")
+plotDay(sun, "Sunday")
+plotDay(mon, "Monday")
+plotDay(tues, "Tuesday")
+plotDay(wed, "Wednesday")
+plotDay(thurs, "Thursday")
 
